5a59dbc0c1b113955c0c8d76a5b960651a188dd0
Cristopher <cristcerpug@gmail.com>
2021-11-23 00:56:46 -0600

    factorial-modification

--- calculator.c
+++ calculator.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 
 int factorial(int x){
-	int fact = 1;
+	long int fact = 1;
 
-	for(int i = 1; i <= x; i++){
-		fact = fact * i;
+	for(int i = x; i > 1; i--){
+		fact *= i;
 	}
 	return fact;
 }
